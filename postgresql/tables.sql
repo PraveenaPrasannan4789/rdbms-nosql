891838466f392062b1430fd82575c15cf83ba774
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-05-23 20:56:43 +0100

    Add purchases table to PostgreSQL schema

--- postgresql/tables.sql
+++ postgresql/tables.sql
@@ -11,3 +11,10 @@ CREATE TABLE orders (
   product VARCHAR(50),
   price DECIMAL(10,2)
 );
+
+CREATE TABLE purchases (
+  purchase_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  product VARCHAR(50),
+  price DECIMAL(10,2)
+);
